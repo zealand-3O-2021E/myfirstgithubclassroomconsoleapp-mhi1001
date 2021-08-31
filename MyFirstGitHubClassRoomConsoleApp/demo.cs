036af1b3001e8bb75e9734e8e46a03fcac9b83fe
pc <pc@DESKTOP-BI5FP4B>
2021-09-01 01:52:55 +0200

    added new class

---
+++ MyFirstGitHubClassRoomConsoleApp/demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    public class demo
+    {
+        private string _name;
+
+        public demo(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+    }
+}
